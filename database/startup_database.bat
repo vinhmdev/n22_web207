dd94fc91276fb8580b032b811cdb3180500adcc6
vinh <mvinhle22@gmail.com>
2021-04-16 13:27:12 +0700

    init

---
+++ database/startup_database.bat
@@ -0,0 +1,5 @@
+echo off
+echo 'Dang chuan bi chay json-server voi file ./database.json'
+echo 'De thoat, an CTRL + C, nhap vao Y, sau do an ENTER'
+start "" http://localhost:3000
+json-server -w ./database.json -p 3000
