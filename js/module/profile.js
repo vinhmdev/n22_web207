dcb3d826ece5a560330e8ef5f5d195ba3740d623
luonghuuloi <82566545+luonghuuloi@users.noreply.github.com>
2021-04-23 14:40:16 +0700

    sửa thông tin (chưa hoàn chỉnh)

---
+++ js/module/profile.js
@@ -0,0 +1,53 @@
+app.controller('profile', function ($scope, $http) {
+    document.cookie.split('; ').forEach(item => {
+        if (item.startsWith('Username')){
+            $scope.usernameRp = (item.substring(item.indexOf('=') + 1));
+        }
+    });
+  
+    $scope.messRp = '';
+
+    $scope.update = function () {
+      
+
+        var user = $scope.usernameRp;
+        var pass = $scope.passRp;
+        var fname = $scope.fnameRp;
+        var email = $scope.emailRp;
+        var phone = $scope.phoneRp;
+
+        $http.get('http://localhost:3000/user?username='+user+'&password='+pass)
+        .then(
+            response => {
+                console.log(response.data)
+                var dataResponse = response.data;
+
+                if(dataResponse.length == 0){
+                    $scope.messRp = 'Sai tài khoản hoặc mật khẩu';
+                    return;
+                }
+
+                var userObj = response.data[0];
+                userObj.password = $scope.newpass1Rp;
+
+                $http.put('http://localhost:3000/user/'+userObj.id, userObj)
+                .then(
+                    response => {
+                        document.cookie = 'LoginId=null;expires=' + new Date(0) + ';path=/';
+                        document.cookie = 'User=null;expires=' + new Date(0) + ';path=/';
+                        document.cookie = 'Pass=null;expires=' + new Date(0) + ';path=/';
+                        document.cookie = 'Username=null;expires=' + new Date(0) + ';path=/';
+                        window.location = "/#!dang-nhap";
+                    },
+                    error => {
+                        console.log(error);
+                        alert('Không thể kết nối đến cơ sỡ dữ liệu');
+                    }
+                );
+            },
+            error => {
+                alert('Không thể kết nối đến cơ sỡ dữ liệu');
+            }
+        );
+    }
+});
